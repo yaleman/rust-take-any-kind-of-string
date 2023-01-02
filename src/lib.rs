cda4f5446a5b150760e1edf3abca78f1eebe0eb0
James Hodgkinson <james@terminaloutcomes.com>
2023-01-03 09:43:31 +1000

    changing to a lib

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+
+/// This takes either a `&'static str` or a `String` and returns a `String`.
+///```
+///use rust_take_any_kind_of_string::takes_any_kind_of_string;
+///
+///fn test_with_static_str() {
+///    let foo = takes_any_kind_of_string("hello world");
+///    assert_eq!(foo, "hello world".to_string());
+///
+///    let bar = takes_any_kind_of_string("hello world".to_string());
+///    assert_eq!(bar, "hello world".to_string());
+///}
+#[allow(dead_code)]
+pub fn takes_any_kind_of_string(value: impl Into<String>) -> String {
+	value.into()
+}
