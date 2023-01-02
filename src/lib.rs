3606df28d2ddc672f9de7894bdc8320e652971d9
James Hodgkinson <james@terminaloutcomes.com>
2023-01-03 09:44:07 +1000

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 /// This takes either a `&'static str` or a `String` and returns a `String`.
 ///```
 ///use rust_take_any_kind_of_string::takes_any_kind_of_string;
@@ -12,5 +11,5 @@
 ///}
 #[allow(dead_code)]
 pub fn takes_any_kind_of_string(value: impl Into<String>) -> String {
-	value.into()
+    value.into()
 }
